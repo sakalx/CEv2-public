278d872e71ac4fd295c2a8a20f82819a1c45584b
serhii-sakal <serhii@carveredison.com>
2019-09-03 18:18:47 -0400

    Updates

---
+++ precache-manifest.61dfd37a9e6d3b407d7a4350c695764c.js
@@ -0,0 +1,357 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b96a0ab5638aec4506fe4adf54c0e84",
+    "url": "/favicon/android-chrome-144x144.png"
+  },
+  {
+    "revision": "7ff455274758df2381701e725d359202",
+    "url": "/favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "f36c993410484c07b86584b342be0fd4",
+    "url": "/favicon/android-chrome-256x256.png"
+  },
+  {
+    "revision": "6613496d5feba55d20fcaec6d0b1f93f",
+    "url": "/favicon/android-chrome-36x36.png"
+  },
+  {
+    "revision": "8cc3c63dbcab671eb87617827cb65584",
+    "url": "/favicon/android-chrome-384x384.png"
+  },
+  {
+    "revision": "58e1e9722092d80e2a398243c1122554",
+    "url": "/favicon/android-chrome-48x48.png"
+  },
+  {
+    "revision": "8476c419f8d29d6b4a70717189eea5dd",
+    "url": "/favicon/android-chrome-512x512.png"
+  },
+  {
+    "revision": "dfa96c276b020a57710b4136871de427",
+    "url": "/favicon/android-chrome-72x72.png"
+  },
+  {
+    "revision": "14b3a109e3385a8eb6abbe78d7673bbb",
+    "url": "/favicon/android-chrome-96x96.png"
+  },
+  {
+    "revision": "fc577cee016aabe2089efc87a533b0ab",
+    "url": "/favicon/apple-touch-icon-1024x1024.png"
+  },
+  {
+    "revision": "132dba358a920bedd7d8f6822e55238b",
+    "url": "/favicon/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "8094c66650d71966e1ecc9a3535bfae9",
+    "url": "/favicon/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "7082b60950aa5705f022ef6bb118cc14",
+    "url": "/favicon/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "e0139de45df9cf609f406acb7e63bc26",
+    "url": "/favicon/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "e6f7117b90e1e55c85567f2f67d34a0e",
+    "url": "/favicon/apple-touch-icon-167x167.png"
+  },
+  {
+    "revision": "6c057c8d198813dfef95d933f8a1af9a",
+    "url": "/favicon/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "dbbf3aa9f6155243984cde8e836f25cb",
+    "url": "/favicon/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "f3350b864e1cb7c0ad9db4b2ec27e13c",
+    "url": "/favicon/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "37233b1ee573de2a082633c4a1f9eeea",
+    "url": "/favicon/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "7b2e0a21860efb3a7faf4d39d7507ad6",
+    "url": "/favicon/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "6c057c8d198813dfef95d933f8a1af9a",
+    "url": "/favicon/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "6c057c8d198813dfef95d933f8a1af9a",
+    "url": "/favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "f9f244517a6165a0e7ab13e5a4b85d29",
+    "url": "/favicon/apple-touch-startup-image-1182x2208.png"
+  },
+  {
+    "revision": "95b34269c7505db562b230ac4a0861e3",
+    "url": "/favicon/apple-touch-startup-image-1242x2148.png"
+  },
+  {
+    "revision": "880518e98a38e82a71791856d8daee97",
+    "url": "/favicon/apple-touch-startup-image-1496x2048.png"
+  },
+  {
+    "revision": "296cad66884c370e505d827c1fae2d76",
+    "url": "/favicon/apple-touch-startup-image-1536x2008.png"
+  },
+  {
+    "revision": "d8d90abfffac5fe1636c8e4c2698f3c0",
+    "url": "/favicon/apple-touch-startup-image-320x460.png"
+  },
+  {
+    "revision": "a8f4e6c4971e060bca370aea5e8ec61c",
+    "url": "/favicon/apple-touch-startup-image-640x1096.png"
+  },
+  {
+    "revision": "a0cbea3cb99e70427d05f22c5b70cc1a",
+    "url": "/favicon/apple-touch-startup-image-640x920.png"
+  },
+  {
+    "revision": "68073d8971421941af5c30768a8e187f",
+    "url": "/favicon/apple-touch-startup-image-748x1024.png"
+  },
+  {
+    "revision": "441dec0ff13933d36d50c28e40c2bcea",
+    "url": "/favicon/apple-touch-startup-image-750x1294.png"
+  },
+  {
+    "revision": "aa3d653fbd4020c0a4895786c46d5d16",
+    "url": "/favicon/apple-touch-startup-image-768x1004.png"
+  },
+  {
+    "revision": "9f1a3d1a44624dcb0e2a03712a746edc",
+    "url": "/favicon/browserconfig.xml"
+  },
+  {
+    "revision": "187349384307b9b070eab40cead9ba7f",
+    "url": "/favicon/coast-228x228.png"
+  },
+  {
+    "revision": "0c2bf38c0a0e32ae73c64dd79b419cd1",
+    "url": "/favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "e2206a97ec0a3171301db99540413a61",
+    "url": "/favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "08bc6d05134ea79d37ea0c31c5209578",
+    "url": "/favicon/favicon.ico"
+  },
+  {
+    "revision": "695d6a43a0661d00fc533aed28bcaaa3",
+    "url": "/favicon/firefox_app_128x128.png"
+  },
+  {
+    "revision": "7ce497e665064ac98cc05054ba75a9e6",
+    "url": "/favicon/firefox_app_512x512.png"
+  },
+  {
+    "revision": "5d705481a3ef8b799dc71b9dcf5cf1e7",
+    "url": "/favicon/firefox_app_60x60.png"
+  },
+  {
+    "revision": "2305af60d28fc33b4508070bd1fcfba6",
+    "url": "/favicon/manifest.json"
+  },
+  {
+    "revision": "54f01167917aef398e5820af15b811b4",
+    "url": "/favicon/manifest.webapp"
+  },
+  {
+    "revision": "3b96a0ab5638aec4506fe4adf54c0e84",
+    "url": "/favicon/mstile-144x144.png"
+  },
+  {
+    "revision": "61096bc54d905e33c4bf04132ee0df28",
+    "url": "/favicon/mstile-150x150.png"
+  },
+  {
+    "revision": "1600f3ed7b648dc20bc3cecaf580130b",
+    "url": "/favicon/mstile-310x150.png"
+  },
+  {
+    "revision": "d6e68bfb397fcafbc2c38818c2128b40",
+    "url": "/favicon/mstile-310x310.png"
+  },
+  {
+    "revision": "57951ab6f554228b790f11f5fd23d97f",
+    "url": "/favicon/mstile-70x70.png"
+  },
+  {
+    "revision": "ce3be4f1c9ea52d5026008b58105ba4d",
+    "url": "/favicon/yandex-browser-50x50.png"
+  },
+  {
+    "revision": "8c297a2763cd1d96913ca44f868a4b1a",
+    "url": "/favicon/yandex-browser-manifest.json"
+  },
+  {
+    "url": "icons/android/icon_144x144.7816c0f74bb6173293536bff3d4f6c88.png"
+  },
+  {
+    "url": "icons/android/icon_192x192.429a20615282f26f731bcc09e2051c5e.png"
+  },
+  {
+    "url": "icons/android/icon_36x36.ba0467add39b58d86211b741ec97a68e.png"
+  },
+  {
+    "url": "icons/android/icon_48x48.16a2033248f40e648fbb5c3b194622dc.png"
+  },
+  {
+    "url": "icons/android/icon_512x512.9a85126bc20aade3bf84052a43c9d0b7.png"
+  },
+  {
+    "url": "icons/android/icon_72x72.7f69dd38b75e4afa565c41ed7171b419.png"
+  },
+  {
+    "url": "icons/android/icon_96x96.4c47363da55ccba22159fbc70cf603de.png"
+  },
+  {
+    "url": "icons/ios/icon_1024x1024.af5eec6da132f37eb1ed906197c8eb37.png"
+  },
+  {
+    "url": "icons/ios/icon_120x120.fece38fd4d7235ae0afce0cef0c60e3a.png"
+  },
+  {
+    "url": "icons/ios/icon_152x152.7e53c56cc1de87a758bab4af6ab6ba9b.png"
+  },
+  {
+    "url": "icons/ios/icon_167x167.33c5c04530d68ff39ad2618793ef7949.png"
+  },
+  {
+    "url": "icons/ios/icon_180x180.efc905064a699b579593b0ecfd6195b8.png"
+  },
+  {
+    "revision": "397813218b19e6ee376c7f27006f6f26",
+    "url": "index.html"
+  },
+  {
+    "revision": "5c94f1e94cdbfeb3c00a",
+    "url": "js/main.bundle.js"
+  },
+  {
+    "url": "manifest.0e1d846d6d579942a61d18efc1e50f10.json"
+  },
+  {
+    "revision": "5fd8f62236485a1f3804e46187b9605a",
+    "url": "static/Advantage.jpg"
+  },
+  {
+    "revision": "27bea6bf76f1cb36a87ca722f06809fa",
+    "url": "static/Aon_Gray.png"
+  },
+  {
+    "revision": "093d85078f8b133c93744b91583365dc",
+    "url": "static/BenefitsPro_Gray.png"
+  },
+  {
+    "revision": "f4fa4f949b5951d6b530829a230d37c3",
+    "url": "static/Bloomberg_Gray.png"
+  },
+  {
+    "revision": "374aec31c30c3c68c1a5c064289e1bdb",
+    "url": "static/CE_Header_Logo.png"
+  },
+  {
+    "revision": "67876fcd298f8fe51ee2c79042942c84",
+    "url": "static/Education_Phone.png"
+  },
+  {
+    "revision": "71682c08a5b33db4438124b2ac103e48",
+    "url": "static/Education_Success.jpg"
+  },
+  {
+    "revision": "fd51140f04f59ff26cf6cc2957802483",
+    "url": "static/Educational.jpg"
+  },
+  {
+    "revision": "ce0c61908c2939e2042930cb0b1ecd18",
+    "url": "static/Employee.png"
+  },
+  {
+    "revision": "bc2e2407a9513b878676ed8d58de2c58",
+    "url": "static/Employees_Laptop1.png"
+  },
+  {
+    "revision": "e65eb3ed41dfb99e3fa3e7686016a287",
+    "url": "static/Employees_Laptop2.png"
+  },
+  {
+    "revision": "49da607daf967746997ca1c251e22c88",
+    "url": "static/Employees_Laptop3.png"
+  },
+  {
+    "revision": "94b98769b0e3917b556231531b5645ad",
+    "url": "static/FA_Gray.png"
+  },
+  {
+    "revision": "6adaf1f1ca35bc13a1df07c90b1d3e3e",
+    "url": "static/Hero.jpg"
+  },
+  {
+    "revision": "aca1382fc436ac09d649d5e2c42cc05a",
+    "url": "static/InvestmentNews_Gray.png"
+  },
+  {
+    "revision": "c691596777dce45fd637071f554fe3e0",
+    "url": "static/LATimes_Gray.png"
+  },
+  {
+    "revision": "935597db2a1eb4f04d2b7e29ad159d9d",
+    "url": "static/Laptop_Companies.png"
+  },
+  {
+    "revision": "99779a7f6c2047934514eb2955c57324",
+    "url": "static/Laptop_Employees.png"
+  },
+  {
+    "revision": "cfc71440758275673269eee35cc27ff7",
+    "url": "static/Logo_Aon_White.png"
+  },
+  {
+    "revision": "2f0b6f958523a6fcafe1dc4e8d90c2bf",
+    "url": "static/Logo_Bloomberg_White.png"
+  },
+  {
+    "revision": "3d6b73315269c3caecb1ba580402fd2b",
+    "url": "static/Logo_InvestmentNews_White.png"
+  },
+  {
+    "revision": "45a30f01e90b9a41c662813552216636",
+    "url": "static/Logo_LATimes_White.png"
+  },
+  {
+    "revision": "e12bc2aae23222df135b572342380fd8",
+    "url": "static/Press_Logos.jpg"
+  },
+  {
+    "revision": "25afe3fd12454dd92fc86b775504d5ff",
+    "url": "static/Press_OneSheeters.jpg"
+  },
+  {
+    "revision": "10380f1226e815c8e45c2e3a7874e9a6",
+    "url": "static/Press_WhitePapers.jpg"
+  },
+  {
+    "revision": "f9206124b041b38c786e68511fc8a04a",
+    "url": "static/TS_Gray.png"
+  },
+  {
+    "revision": "408cb8aa0a88da78f1ff81dc89ad525f",
+    "url": "static/Team.jpg"
+  },
+  {
+    "revision": "60f104dc19b47039dbc16854abf89b46",
+    "url": "static/ZeroHedge_Gray.png"
+  }
+]);
